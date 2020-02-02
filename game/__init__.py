1be9bc805535982ed939e713fef0575408e3e08a
colorfulcodes <codescolorful@gmail.com>
2020-02-01 21:41:39 -0500

    feat: added large ascii font

--- game/__init__.py
+++ game/__init__.py
@@ -1,6 +1,9 @@
 from easy import easy
 from medium import medium
 from hard import hard
+from threading import Timer
+from time import sleep
+import pyfiglet
 import pygame
 import time
 import os
@@ -9,6 +12,8 @@ import os
 def welcomeMessage():
     # To hide pygame intro
     os.system('clear')
+    result = pyfiglet.figlet_format("Master Mind!") 
+    print(result)
     print(
     'Welcome to MasterMind!\n\n\nThe rules are: '
     'The computer will choose four random numbers in astring.\n'
@@ -31,10 +36,12 @@ def welcomeMessage():
 def difficulty(choice):
     if choice == 'easy':
         value= easy()
+        print(value)
         playing(value)
 
     if choice == 'medium':
         value= medium()
+        print(value)
         playing(value)
 
     if choice == 'hard':
@@ -42,15 +49,33 @@ def difficulty(choice):
         playing(value)
     
 
-def playing(num):
+def playing(numb):
+    num = numb[0]
     guesses=[]
     count = 1
+    timeout = 3
     print('Time to guess a four digit number.You have 10 attempts: ')
     while len(guesses)<1:
         print('Your guesses: '+ str(guesses))
+        # if timeout > 0:
+        #     t = Timer(timeout, print, ['Sorry, times up. Press enter'])
+        #     t.start()
+        #     prompt = "You have %d seconds to choose the correct answer...\n" % timeout
+        #     guess = input(prompt)
+        #     guesses.append(guess)
+        #     if count>=3:
+        #         count -=2
+        #     else:
+        #         count-=1
+
+        # elif timeout==0:
         guess=input()
         guesses.append(int(guess))
+
         if guess == num:
+            os.system('clear')
+            print(pyfiglet.figlet_format("WINNER!"))
+            print('\n')
             print(num+ ' is the correct answer! You have won!')
             sound('soundEffects/yay.wav')
             return
@@ -59,8 +84,12 @@ def playing(num):
             count-= 1
             print('You have '+ str(count) +" guess(es) left.")
     print(guesses)
+    os.system('clear')
     print("Sorry, you are out of guesses. Goodbye.")
+    sleep(1)
     # MP3
+
+    print(pyfiglet.figlet_format("LOSER!"))
     sound('soundEffects/womp.wav')
     return
 
